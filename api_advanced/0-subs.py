cf0eee64e0f4f1948e95b20b42f1fbbf126339a2
TKcodes-bit <t.odongo@alustudents.com>
2025-07-08 10:59:54 +0300

    0-subs.py

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fetches number of subscribers for a given subreddit."""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "CustomRedditClient/1.0"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("data", {}).get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0

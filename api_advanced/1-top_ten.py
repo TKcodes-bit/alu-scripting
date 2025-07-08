7ada6ac62b8f8432f9d5068a21c6382df735dbd1
TKcodes-bit <t.odongo@alustudents.com>
2025-07-08 16:30:35 +0300

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""Fetches and prints the titles of the top 10 hot posts for a given subreddit."""
+"""Fetch and print the titles of the first 10 hot posts on a subreddit."""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a subreddit."""
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "HolbertonSchoolProject"}
-    params = {"limit": 10}
+    headers = {"User-Agent": "HolbertonSchool"}
 
-    try:
-        res = requests.get(url, headers=headers,
-                           params=params, allow_redirects=False)
-        if res.status_code != 200:
-            print("None")
-            return
+    response = requests.get(
+        url, headers=headers, params={"limit": 10}, allow_redirects=False
+    )
 
-        posts = res.json().get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-
-    except Exception:
+    if response.status_code != 200:
         print("None")
+        return
+
+    data = response.json().get("data")
+    if not data:
+        print("None")
+        return
+
+    posts = data.get("children")
+    if not posts:
+        print("None")
+        return
+
+    for post in posts:
+        print(post.get("data").get("title"))
